3be7d5b6a955324045cc62c32c9dd5f342468d20
Shab-33 <147744448+Shab-33@users.noreply.github.com>
2025-02-17 12:27:31 +0000

    Added ApplicationDBContext.cs

---
+++ Next2-BlazorServerApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Next2_BlazorServerApplication.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options) { }
+
+		public DbSet<Question> Questions { get; set; }
+		public DbSet<Answer> Answers { get; set; }
+	}
+
+	public class Question
+	{
+		public int QuestionID { get; set; }
+		public string QuestionText { get; set; }
+		public int? ParentQuestionID { get; set; }
+		public string Category { get; set; }
+		public List<Answer> Answers { get; set; }
+	}
+
+	public class Answer
+	{
+		public int AnswerID { get; set; }
+		public int QuestionID { get; set; }
+		public string AnswerText { get; set; }
+		public int? NextQuestionID { get; set; }
+	}
+}
--- Next2-BlazorServerApplication/Program.cs
+++ Next2-BlazorServerApplication/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Next2_BlazorServerApplication.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
